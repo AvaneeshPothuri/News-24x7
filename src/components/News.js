d95179d0c8617acee167bd3b653c9fc689029115
AvaneeshPothuri <122687950+AvaneeshPothuri@users.noreply.github.com>
2024-12-24 15:31:05 +0530

    sixth

--- src/components/News.js
+++ src/components/News.js
@@ -1,40 +1,72 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner'
 
 export class News extends Component {
-  constructor(){
+  constructor() {
     super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e6ebfd796ed14871a7d45fc3e85d68e5&page=1&pageSize=${this.props.pageSize}`
+    this.setState({loading: true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({loading: false}) 
+    this.setState({ articles: parsedData.articles , totalResults: parsedData.totalResults });
+  }
+
+  handleNextClick = async()=> {
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e6ebfd796ed14871a7d45fc3e85d68e5&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({loading: false})
+    this.setState({ articles: parsedData.articles });
+    this.setState({ page: this.state.page + 1})  
+  }
+
+  handlePreviousClick = async()=> {
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e6ebfd796ed14871a7d45fc3e85d68e5&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
+    this.setState({loading: true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({loading: false})
+    this.setState({ articles: parsedData.articles });
+    this.setState({ page: this.state.page - 1 })  
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h2>Top Headlines</h2>
+        <h2 className='text-center'>Top Headlines</h2>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="My Title" description="My Description" imageURL=".."/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="My Title" description="My Description"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="My Title" description="My Description"/>
-          </div>
+          {!this.state.loading && this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem 
+                  title={element.title ? element.title:""} 
+                  description={element.description ? element.description:""} 
+                  imageURL={element.urlToImage?element.urlToImage:""} 
+                  newsURL={element.url} 
+                />
+              </div>
+            );
+          })}
         </div>
-        <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="My Title" description="My Description"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="My Title" description="My Description"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="My Title" description="My Description"/>
-          </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page===1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
+          <button disabled={Math.ceil(this.state.totalResults/this.props.pageSize)===this.state.page} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title , description , imageURL } = this.props;
+    let { title, description, imageURL, newsURL } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-            <img src={imageURL} className="card-img-top" alt='hi' />
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/" className="btn btn-sm btn-primary">Go somewhere</a>
-            </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageURL} className="card-img-top" alt="hi" />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <button 
+              className="btn btn-sm btn-dark" 
+              onClick={() => window.open(newsURL, "_blank")}
+            >
+              Read More
+            </button>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
